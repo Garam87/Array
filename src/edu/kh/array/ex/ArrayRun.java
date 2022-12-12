db99ce40d2aa819dd5f080f7dbcdf723ed07640e
garam <dheotjd87@gmail.com>
2022-12-12 15:15:40 +0900

    first

---
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,19 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayExample1 ae = new ArrayExample1();
+		
+//		ae.ex1();
+//		ae.ex2();
+//		ae.ex3();
+//		ae.ex4();
+//		ae.ex5();
+//		ae.ex6();
+		ae.ex7();
+	}
+
+}
